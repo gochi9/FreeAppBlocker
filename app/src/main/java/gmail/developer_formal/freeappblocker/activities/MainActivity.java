5a549be4188ecd0288148bdfd7a8a09634a24c15
gochi9 <gochijoaca@yahoo.com>
2024-05-23 20:29:46 +0300

    Multi-language support

--- app/src/main/java/gmail/developer_formal/freeappblocker/activities/MainActivity.java
+++ app/src/main/java/gmail/developer_formal/freeappblocker/activities/MainActivity.java
@@ -1,13 +1,9 @@
 package gmail.developer_formal.freeappblocker.activities;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
